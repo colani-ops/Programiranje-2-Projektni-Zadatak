73b438ef2c5b20a1e970ce68c97b4d2e31968fb5
colani-ops <84501234+colani-ops@users.noreply.github.com>
2021-05-27 08:48:10 +0200

    Update header.h

--- header.h
+++ header.h
@@ -5,25 +5,38 @@
 
 typedef struct SpaceMarine {
 
-        char name[64];
-        char rank[32];
-        char company[32];
-        int age;
-        int yearsOfService;
-        char speciality[16];
+	char name[64];
+	char rank[32];
+	char company[32];
+	int age;
+	int yearsOfService;
+	char speciality[16];
 
 }SpaceMarine;
 
 typedef struct SpaceMarineSquad {
 
-        int squadNumber;
-        int unitCount;
-        char* squadType;
+	int squadNumber;
+	int marineCount;
+	char squadType[16];
 
-        struct SpaceMarine;
+	struct SpaceMarine* marineArray;//ovdje dinamicki alocirati broj marinaca u squadu sa marine count prilikom dodavanja.
+									//Mozda napraviti da se dodaje squad po squad a ne marine po marine.
+
 
 }SMSquad;
 
-int provjeraDatoteke(char imeDatoteke);
+typedef struct SpaceMarineUnit {
+
+	int sAmount;
+
+	struct SpaceMarineSquad* squads;
+
+}SMunit;
+
+int provjeraDatoteke(char* imeDatoteke);
+SMSquad* dodavanjeSquada(SMunit* unit);
+SMSquad* sRead(char* imeDatoteke);
+void sWrite(char* imeDatoteke, SMSquad* outArray, int sizeOfOutArray);
 
 #endif //header_h
