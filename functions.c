a32f91633a76951cda8d82d4fab889c3298e407a
colani-ops <84501234+colani-ops@users.noreply.github.com>
2021-05-27 08:47:48 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -4,63 +4,136 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+/*Polje strutura (squadova) koja u sebi imaju vise space marinaca,
+prilikom saveanja overwriteat sve u postojecoj datoteci*/
 
 int provjeraDatoteke(char* imeDatoteke) {
 
-        FILE* input = fopen(imeDatoteke, "rb+");
+	FILE* input = fopen(imeDatoteke, "rb+");
 
-        printf("Funkcija provjera datoteke");
+	printf("Funkcija provjera datoteke");
 
-        if (input == NULL) {
-                printf("\nNe postoji datoteka, kreiranje datoteke...");
+	if (input == NULL) {
+		printf("\nNe postoji datoteka, kreiranje datoteke...");
 
-                FILE* input = fopen(imeDatoteke, "wb+");
+		FILE* input = fopen(imeDatoteke, "wb+");
 
-                if (input == NULL) {
-                        printf("\nGreska u kreiranju datoteke");
-                        exit(EXIT_FAILURE);
-                }
+		if (input == NULL) {
+			printf("\nGreska u kreiranju datoteke");
+			exit(EXIT_FAILURE);
+		}
 
-                fclose(input);
+		fclose(input);
+	}
 
-        }
+	else {
 
-        else {
+		fclose(input);
 
-                fclose(input);
 
-                return 1;
-        }
+	}
+	return 1;
+
+}
+
+
+
+SMSquad* dodavanjeSquada(SMunit* unit) {
+
+	unit->squads = (SMSquad*)realloc(unit->squads, unit->sAmount + 1);
+
+
+	printf("\nUnesite squad number : ");
+	scanf("%d", &unit->squads[sAmount].squadNumber);
+	printf("\nUnesite marine count : ");
+	scanf("%d", &unit->squads[sAmount].marineCount);
+	printf("\nUnesite squad type");
+	scanf("%16s", unit->squads[sAmount].squadType);
+
+	for (int i = 0; squads->marineCount; i++) {
+
+		
+
+		return squads;
+
+	}
 
 }
 
-void dodavanje(char* imeDatoteke) {
 
-        FILE* input = fopen(imeDatoteke, "wb+");
 
-        if (input == NULL) {
-                perror("Dodavanje u datoteku.");
-                exit(EXIT_FAILURE);
-        }
+void inputSquada() {
 
-        SpaceMarine temp;
+	int i;
+	int j;
+	
+	for (i = 0; i < sAmount; i++) {
 
-        printf("\nUnesite ime : ");
-        scanf("%63[^\n]", temp.name);
-        printf("\nUnesite rank : ");
-        scanf("%31[^\n]", temp.rank);
-        printf("\nUnesite company : ");
-        scanf("%31[^\n]", temp.company);
-        printf("\nUnesite age : ");
-        scanf("%d", &temp.age);
-        printf("\nUnesite years of service : ");
-        scanf("%d", &temp.yearsOfService);
-        printf("\nUnesite speciality");
-        scanf("%15[^\n]", temp.speciality);
+		printf("\n\nUnesite squad number : ");
 
+		printf("\nUnesite marine count : ");
+		
+		printf("\nUnesite squad type : ");
+
+
+		for (j = 0; j < squads.marineCount; j++) {
+
+			printf("\nUnesite ime : ");
+			scanf("%63[^\n]", SMunit->squads[sAmount].marineArray[j].name);
+			printf("\nUnesite rank : ");
+			scanf("%31[^\n]", SMunit->squads[sAmount].marineArray[j].rank);
+			printf("\nUnesite company : ");
+			scanf("%31[^\n]", SMunit->squads[sAmount].marineArray[j].company);
+			printf("\nUnesite age : ");
+			scanf("%d", SMunit->squads[sAmount].marineArray[j].age);
+			printf("\nUnesite years of service : ");
+			scanf("%d", SMunit->squads[sAmount].marineArray[j].yearsOfService);
+			printf("\nUnesite speciality");
+			scanf("%15[^\n]", SMunit->squads[sAmount].marineArray[j].speciality);
+
+		}
+
+	}
 
 }
+
+
+SMSquad* sRead(char* imeDatoteke) {
+
+	FILE* input = fopen(imeDatoteke, "rb");
+	if (input == NULL) {
+		exit(EXIT_FAILURE);
+	}
+
+	int sAmount;
+	fread(&sAmount, sizeof(int), 1, input);
+
+	SMSquad* squadArray = (SMSquad*)malloc(sAmount * sizeof(SMSquad));
+
+	fread(squadArray, sizeof(SMSquad), sAmount, input);
+
+	fclose(input);
+
+	return squadArray;
+
+}
+
+
+
+void sWrite(char* imeDatoteke, SMSquad* outArray, int sizeOfOutArray) {
+
+	FILE* output = fopen(imeDatoteke, "wb");
+	if (output == NULL) {
+		exit(EXIT_FAILURE);
+	}
+
+	fwrite(&sizeOfOutArray, sizeof(int), 1, output);
+	fwrite(outArray, sizeof(SMSquad), sizeOfOutArray, output);
+	fclose(output);
+}
+
+
 
 void zatvaranje() {
-        exit(EXIT_SUCCESS);
+	exit(EXIT_SUCCESS);
 }
