85a7df7ab7afbeb8426903f74c0f43a8a47a6ea7
colani-ops <84501234+colani-ops@users.noreply.github.com>
2021-07-14 13:39:47 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main() {
 	char imeDatoteke[20] = { '\0' };
 	printf("\nObavezno dodajte .bin na kraju imena datoteke.");
 	printf("\nUnesite ime datoteke u program : ");
-	scanf("%s", imeDatoteke);
+	scanf("%19[^\n]", imeDatoteke);
 	//fgets(imeDatoteke,20,stdin)
 
 	int status = provjeraDatoteke(imeDatoteke);
